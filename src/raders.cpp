8a30aeffd065236360030e0457e2e3940aeeda62
Dominic Marcello <dmarce504@gmail.com>
2023-04-17 23:08:39 -0500

    raders

---
+++ src/raders.cpp
@@ -0,0 +1,55 @@
+#include "fftu.hpp"
+#include "util.hpp"
+#include <cstring>
+
+template<class T>
+void fft_raders1(complex<T>* X, int N) {
+	std::vector<complex<T>> Y(N - 1);
+	const auto& gq = raders_gq(N);
+	const auto& ginvq = raders_ginvq(N);
+	const auto& tw = raders_twiddle(N);
+	complex<T> xo = X[N - 1];
+	complex<T> x0 = xo;
+	for (int n = 0; n < N - 1; n++) {
+		x0 += X[n];
+	}
+	for (int n = 0; n < N - 1; n++) {
+		Y[n] = X[n];
+	}
+	fft(Y.data(), N - 1);
+	for (int q = 0; q < N - 1; q++) {
+		Y[q] *= tw[q];
+	}
+	for (int q = 1; q < N - 1 - q; q++) {
+		std::swap(Y[q], Y[N - 1 - q]);
+	}
+	fft_scramble(Y.data(), N - 1);
+	fft(Y.data(), N - 1);
+	for (int p = 0; p < N - 1; p++) {
+		Y[p] += xo;
+	}
+	X[0] = x0;
+	for (int p = 0; p < N - 1; p++) {
+		X[ginvq[p]] = Y[p];
+	}
+}
+
+void fft_raders_indices(int* I, int N) {
+	std::vector<int> J(N);
+	const auto& gq = raders_gq(N);
+	for (int n = 0; n < N - 1; n++) {
+		J[n] = I[gq[n]];
+	}
+	J[N - 1] = I[0];
+	std::memcpy(I, J.data(), N * sizeof(int));
+	fft_indices(I, N - 1);
+}
+
+void fft_raders(complex<fft_simd4>* X, int N) {
+	fft_raders1<fft_simd4>(X, N);
+}
+
+void fft_raders(complex<double>* X, int N) {
+	fft_raders1<double>(X, N);
+}
+
