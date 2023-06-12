fa7358f0d7f019b898b6635d2fed159cef8ba456
Dominic Marcello <dmarce504@gmail.com>
2023-06-12 10:49:54 -0500

    fixed bug

--- src/main.cpp
+++ src/main.cpp
@@ -91,10 +91,10 @@ int main(int argc, char **argv) {
 				std::vector<complex<double>> X(N / 2 + 1);
 				std::vector<complex<double>> Y(N / 2 + 1);
 				for (int n = 0; n < N; n++) {
-					x[n] = (y[n] = rand1());
+					x[n] = (y[n] = 0);
 				}
 				//x[0] = y[0] = 1.0;
-//				x[0] = y[0] = 1.0;
+				x[0] = y[0] = 1.0;
 				if (i == 0) {
 					fftw_real(Y, y);
 					fft_inplace_real(x.data(), N);
