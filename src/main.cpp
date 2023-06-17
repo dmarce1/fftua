1c1ec0f61032b23bd55a52b73cd194b44816dc29
Dominic Marcello <dmarce504@gmail.com>
2023-06-17 12:02:09 -0500

    fi

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,7 @@ int main(int argc, char **argv) {
 	std::vector<int> Ns;
 	double score = 0.0;
 	int cnt = 0;
-	for (int N = 64; N <= 4*1024*1024; N *= 4) {
+	for (int N = 256; N <= 4*1024*1024; N *= 4) {
 		auto pfac = prime_factorization(N);
 		{
 			double avg_err = 0.0;
@@ -154,7 +154,7 @@ int main(int argc, char **argv) {
 				std::vector<complex<double>> X(N / 2 + 1);
 				std::vector<complex<double>> Y(N / 2 + 1);
 				for (int n = 0; n < N; n++) {
-					x[n] = (y[n] =0);
+					x[n] = (y[n] =rand1());
 				}
 				x[0] = y[0] = 1.0;
 //				x[0] = y[0] = 1.0;
