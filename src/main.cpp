9690ae0f9e14d70d937ef8938f705b4fd69fb169
Dominic Marcello <dmarce504@gmail.com>
2023-06-30 06:45:20 -0500

    2

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ void twiddle_gen_next(__m256d* C, __m256d* S, void *ptr);
 void twiddle_gen_init(void* ptr, int N);
 void fft_simd_scramble(double*, int N);
 void fft_transpose_hilo(double*, int, int);
-void fft_recursive(double* X, const double* C, const double* S, int N);
+void fft_recursive(double* X, const double* C, int N);
 }
 
 
@@ -147,7 +147,7 @@ int main(int argc, char **argv) {
 	std::vector<int> Ns;
 	double score = 0.0;
 	int cnt = 0;
-	for (int N = 16; N <= 4*1024*1024; N *= 2) {
+	for (int N = 64; N <= 4*1024*1024; N *= 2) {
 		auto pfac = prime_factorization(N);
 		{
 			double avg_err = 0.0;
@@ -161,19 +161,19 @@ int main(int argc, char **argv) {
 				for (int n = 0; n < N; n++) {
 					x[n] = (y[n] =rand1());
 				}
-				x[16] = y[16] = 1.0;
+			//	x[16] = y[16] = 1.0;
 //				x[0] = y[0] = 1.0;
 				const auto& c = cos_twiddles(N);
 				const auto& s = sin_twiddles(N);
 				if (i == 0) {
 					fftw_real(Y, y);
-					fft_recursive(x.data(), c.data(), s.data(), N);
+					fft_recursive(x.data(), c.data(), N);
 				} else {
 
 					auto b = fftw_real(Y, y);
 					timer tm;
 					tm.start();
-					fft_recursive(x.data(), c.data(), s.data(), N);
+					fft_recursive(x.data(), c.data(), N);
 					//test_time(x.data(), N);
 					tm.stop();
 					X[0].real() = x[0];
