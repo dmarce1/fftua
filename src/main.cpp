118f2c8a93b8465fa5bd79299406d62a822bb3b3
Dominic Marcello <dmarce504@gmail.com>
2023-07-08 04:18:23 -0500

    ?

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,8 @@ void twiddle_gen_next(__m256d* C, __m256d* S, void *ptr);
 void twiddle_gen_init(void* ptr, int N);
 void fft_simd_scramble(double*, int N);
 void fft_transpose_hilo(double*, int, int);
-void dit_nr_recur(double* X, int N);
+void fft_recursive(double* X, const double* C, int N);
+void dit_nr_recur(double* X,int N);
 }
 
 
@@ -108,7 +109,7 @@ void test_twiddles() {
 void test_time(double* x, int N) {
 //	fft_simd_scramble(x, N);
 //	fft_simd_scramble(x, N);
-//	fft_scramble(x, N);
+	fft_scramble(x, N);
 }
 
 extern "C" {
@@ -153,27 +154,27 @@ int main(int argc, char **argv) {
 			double avg_err = 0.0;
 			double t1 = 0.0;
 			double t2 = 0.0;
-			for (int i = 0; i < 2; i++) {
+			for (int i = 0; i < 101; i++) {
 				std::vector<double> x(N);
 				std::vector<double> y(N);
 				std::vector<complex<double>> X(N / 2 + 1);
 				std::vector<complex<double>> Y(N / 2 + 1);
 				for (int n = 0; n < N; n++) {
-					//x[n] = (y[n] =rand1());
+					x[n] = (y[n] =rand1());
 				}
-				x[0] = y[0] = 1.0;
-			//x[6] = y[6] = 1.0;
+			//	x[16] = y[16] = 1.0;
+//				x[0] = y[0] = 1.0;
 				const auto& c = cos_twiddles(N);
 				const auto& s = sin_twiddles(N);
 				if (i == 0) {
 					fftw_real(Y, y);
-					dit_nr_recur(x.data(), N);
+					dit_nr_recur(x.data(),N);
 				} else {
 
 					auto b = fftw_real(Y, y);
 					timer tm;
 					tm.start();
-					dit_nr_recur(x.data(), N);
+					dit_nr_recur(x.data(),  N);
 					//test_time(x.data(), N);
 					tm.stop();
 					X[0].real() = x[0];
@@ -195,11 +196,11 @@ int main(int argc, char **argv) {
 						double y = X[n].imag() - Y[n].imag();
 						double err = sqrt(x * x + y * y);
 						avg_err += err;
-						printf("%16e %16e | %16e %16e | %16e %16e\n", X[n].real(), X[n].imag(), Y[n].real(), Y[n].imag(), X[n].real() - Y[n].real(), X[n].imag() - Y[n].imag());
+		//				printf("%16e %16e | %16e %16e | %16e %16e\n", X[n].real(), X[n].imag(), Y[n].real(), Y[n].imag(), X[n].real() - Y[n].real(), X[n].imag() - Y[n].imag());
 					}
 				}
 			}
-			abort();
+		//	abort();
 			std::string f;
 			for (auto i = pfac.begin(); i != pfac.end(); i++) {
 				f += "(" + std::to_string(i->first) + "^" + std::to_string(i->second) + ")";
