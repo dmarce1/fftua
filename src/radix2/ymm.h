9e9945fa23c6283b3b152f6530734742a8e059fa
Dominic Marcello <dmarce504@gmail.com>
2023-07-10 04:57:44 -0500

    Working

---
+++ src/radix2/ymm.h
@@ -0,0 +1,39 @@
+/*
+ * ymm.h
+ *
+ *  Created on: Jul 10, 2023
+ *      Author: dmarce1
+ */
+
+#ifndef YMM_H_
+#define YMM_H_
+
+
+#define       er0            %ymm0
+#define       er1            %ymm1
+#define       er2            %ymm2
+#define       er3            %ymm3
+#define       ei0            %ymm4
+#define       ei1            %ymm5
+#define       ei2            %ymm6
+#define       ei3            %ymm7
+#define       tr0            %ymm8
+#define       tr1            %ymm9
+#define       tr2            %ymm10
+#define       tr3            %ymm11
+#define       ti0            %ymm12
+#define       ti1            %ymm13
+#define       ti2            %ymm14
+#define       ti3            %ymm15
+#define       cos1           %ymm8
+#define       sin1           %ymm9
+#define       cos2           %ymm10
+#define       sin2           %ymm11
+#define       tr             %ymm12
+#define       ti             %ymm13
+#define       two            %ymm14
+#define       half           %ymm15
+
+
+
+#endif /* YMM_H_ */
